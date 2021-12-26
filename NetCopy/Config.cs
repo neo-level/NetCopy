65e726e3cb378a73b7ec0cbba3853c71eaa1c504
neo-level <info@neo-level.com>
2021-12-26 18:53:02 +0100

    Finished the parsing for the cli input.

--- NetCopy/Config.cs
+++ NetCopy/Config.cs
@@ -1,6 +1,59 @@
-﻿namespace NetCopy;
+﻿using System.Net;
 
-public class Config
+namespace NetCopy;
+
+/// <summary>
+/// Parses the cli arguments.
+/// </summary>
+internal class Config
 {
-    
+    public string? FileName { get; }
+    public int Port { get; } = 9021;
+    public bool IsServer { get; } = true;
+    public bool Debug { get; }
+    public IPAddress ServerIp { get; }
+    public bool AskForHelp { get; }
+
+    public Config(string?[] args)
+    {
+        ServerIp = IPAddress.Any;
+        if (args.Length == 0) return;
+
+        int index = 0;
+        while (index < args.Length)
+        {
+            if (IsMatch(args[index], "-ip", "/ip", "ip"))
+            {
+                index++;
+                ServerIp = IPAddress.Parse(args[index] ?? "No valid IP was given.");
+                IsServer = false;
+            }
+            else if (IsMatch(args[index], "-p", "/p", "port"))
+            {
+                index++;
+                Port = int.Parse(args[index] ?? "No valid Port was given.");
+            }
+            else if (IsMatch(args[index], "-h", "/h", "/help", "help"))
+            {
+                AskForHelp = true;
+                return;
+            }
+            else if (IsMatch(args[index], "-d", "/d"))
+            {
+                Debug = true;
+            }
+            else
+            {
+                FileName = args[index];
+            }
+
+            index++;
+        }
+    }
+
+    private bool IsMatch(string? leftHand, params string[] rightHand)
+    {
+        var match = rightHand.FirstOrDefault(s => s == leftHand?.ToLower());
+        return !string.IsNullOrEmpty(match);
+    }
 }
