af0c5727b7fa00d272a1ecab108900d7807d14f8
neo-level <info@neo-level.com>
2021-12-26 18:55:27 +0100

    Finished main program, based on arguments, the app acts as a client or server.

--- NetCopy/Program.cs
+++ NetCopy/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿namespace NetCopy;
+
+internal static class Program
+{
+   static void Main(string?[] args)
+   {
+      var config = new Config(args);
+
+      if (config.AskForHelp || string.IsNullOrEmpty(config.FileName))
+      {
+         Console.WriteLine("NetCopy");
+         Console.WriteLine("A simple peer to peer copy between machines.");
+         Console.WriteLine("\tSend\tncp [-ip serverIP] [-p port] filename");
+         Console.WriteLine("\tReceive\tncp [-p port] filename");
+      }
+      else if (config.IsServer)
+      {
+         var server = new Receiver(config.ServerIp, config.Port, config.FileName, config.Debug);
+         server.Run();
+      }
+      else
+      {
+         var sender = new Sender(config.ServerIp, config.Port, config.FileName);
+         sender.Run();
+      }
+   } 
+}
